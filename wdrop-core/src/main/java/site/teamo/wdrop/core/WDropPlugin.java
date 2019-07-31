7bfbcf2c10e8e0161fc5b48d13204f883f54f7ff
haocongshun <haocongshun@hikvision.com.cn>
2019-07-31 21:51:37 +0800

    [MOD]调整WDropPlugin代码

--- wdrop-core/src/main/java/site/teamo/wdrop/core/WDropPlugin.java
+++ wdrop-core/src/main/java/site/teamo/wdrop/core/WDropPlugin.java
@@ -9,26 +9,46 @@ import site.teamo.wdrop.bean.Plugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.charset.Charset;
 
 public class WDropPlugin {
     private static final Logger LOGGER = LoggerFactory.getLogger(WDropPlugin.class);
 
-    public void initPlugin(String pluginPath){
-        File[] plugins = new File(pluginPath).listFiles();
-        for(File plugin : plugins){
-            File[] pluginInfoFile = plugin.listFiles((dir,name)->name.endsWith(".json"));
-            if(pluginInfoFile==null||pluginInfoFile.length!=1){
-                continue;
-            }
-            try {
-                String pluginJson = FileUtils.readFileToString(pluginInfoFile[0], Charset.forName("UTF-8"));
-                Plugin pluginBean = JSON.parseObject(pluginJson,Plugin.class);
-                pluginBean.setLibPath(plugin.getPath());
-            } catch (IOException e) {
-                LOGGER.error("read plugin info error,{}",pluginInfoFile[0].getPath());
-                continue;
-            }
+    public enum WDropPluginStatus{
+        GREEN,
+        YELLOW,
+        RED;
+    }
+
+    private Plugin plugin;
+    private WDropPluginStatus status;
+    private WDropClassLoader wDropClassLoader;
+
+    private WDropPlugin(){}
+
+    public static WDropPlugin createWDropPlugin(Plugin plugin){
+        WDropPlugin wDropPlugin = new WDropPlugin();
+        wDropPlugin.status = WDropPluginStatus.GREEN;
+        try {
+            wDropPlugin.wDropClassLoader = WDropClassLoader.newWDropClassLoader(plugin.getLibPath(), Thread.currentThread().getContextClassLoader());
+        } catch (Exception e) {
+            LOGGER.error("create WDropClassLoader error,plugin:{}",JSON.toJSONString(plugin));
+            wDropPlugin.status=WDropPluginStatus.YELLOW;
         }
+        wDropPlugin.plugin = plugin;
+        return wDropPlugin;
+    }
+
+    public Plugin getPlugin() {
+        return plugin;
+    }
+
+    public WDropPluginStatus getStatus() {
+        return status;
+    }
+
+    public WDropClassLoader getwDropClassLoader() {
+        return wDropClassLoader;
     }
 }
