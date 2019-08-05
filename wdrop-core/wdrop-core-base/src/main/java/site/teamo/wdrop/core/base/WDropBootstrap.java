33b37bd86638c7336a17640738d48a8780846b28
haocongshun <haocongshun@hikvision.com.cn>
2019-08-05 16:22:15 +0800

    [MOD]调整打包的配置

--- wdrop-core/wdrop-core-base/src/main/java/site/teamo/wdrop/core/base/WDropBootstrap.java
+++ wdrop-core/wdrop-core-base/src/main/java/site/teamo/wdrop/core/base/WDropBootstrap.java
@@ -1,19 +1,28 @@
 package site.teamo.wdrop.core.base;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import site.teamo.wdrop.core.util.SpringUtil;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class WDropBootstrap {
 
+    @Autowired
+    private WDropPluginDataSource wDropPluginDataSource;
 
     /**
      * 核心启动引导
      * @param args
      */
     public static void main(String[] args){
+
+    }
+
+    @PostConstruct
+    public void init(){
         WDropContainer wDropContainer = WDropContainer.getInstance();
-        WDropPluginDataSource wDropPluginDataSource = SpringUtil.getApplicationContext().getBean("wDropPluginDataSource",WDropPluginDataSource.class);
         wDropContainer.setWDropPluginDataSource(wDropPluginDataSource);
         wDropContainer.load();
     }
